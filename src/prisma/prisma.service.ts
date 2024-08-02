ff1e68aec22613b251e93dabbaadf735eaad938d
Victor Ribeiro <jvictorjy@gmail.com>
2024-08-02 19:26:17 -0300

    feat: initial commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'warn' },
+        { emit: 'event', level: 'error' },
+      ],
+    })
+  }
+
+  onModuleDestroy() {
+    return this.$connect()
+  }
+
+  onModuleInit() {
+    return this.$disconnect()
+  }
+}
